f614f93ce1ab29f0edafe694e027f0d269d6cb25
waruna-lakshantha <waruna.lakshantha@gmail.com>
2023-01-31 09:23:13 +0530

    With unit test

--- be_api/tests/unit/model/doctrine/UserTableTest.php
+++ be_api/tests/unit/model/doctrine/UserTableTest.php
@@ -1,24 +1,68 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
-class UserTableTest extends TestCase
+class UserTableTest extends PHPUnit_Framework_TestCase
 {
-    public function testLoginSuccess()
+
+    private $http;
+    private $url;
+
+    public function setUp()
     {
-        $para = array("token"=>"hrm_api", "user_name"=>"waruna", "password"=>"Waruna123");
+        $this->http = new GuzzleHttp\Client([
+            'headers' => [ 'Content-Type' => 'application/json' ]
+        ]); 
+        
+        $this->url = 'http://ohrmapi:8089/backend_dev.php/api/auth.json';
+    }    
 
-        $instantiator = new \Doctrine\Instantiator\Instantiator();
+    public function testLoginSuccess()
+    {        
+        $response = $this->http->post($this->url,
+            ['body' => json_encode(
+                [
+                    'token' => 'hrm_api',
+                    'user_name' => 'waruna',
+                    'password' => 'Waruna123'
+                ]
+            )]
+        );
 
-        $UserTable = $instantiator->instantiate('UserTable::class');
+        //Unit Test : Successful login
+        $this->assertEquals(200, $response->getStatusCode());
 
-        $empAct = $UserTable->getForToken($para);
+        $data = json_decode($response->getBody(), true);
 
-        $q = Doctrine_Query::create()
-        ->from('Employee e')
-        ->where('e.id = ?', 1);
-  
-        $empExpected = $q->fetchOne();        
-
-        $this->assertEquals($empExpected, $empAct);
+        //Unit Test : Access of employee data for a logged-in use
+        $this->assertArrayHasKey('user_name', $data[0]);
+        $this->assertArrayHasKey('id', $data[0]);  
+        $this->assertArrayHasKey('first_name', $data[0]);
+        $this->assertArrayHasKey('last_name', $data[0]);  
+        $this->assertArrayHasKey('address', $data[0]);   
     }
+
+    public function testLoginFail()
+    {
+        $response = $this->http->post($this->url,
+            ['body' => json_encode(
+                [
+                    'token' => 'hrm_api',
+                    'user_name' => 'waruna1',
+                    'password' => 'Waruna123'
+                ]
+            )]
+        );
+
+        //Unit Test : Failed login
+        $this->assertEquals(404, $response->getStatusCode());
+
+        //Unit Test : Failed login Message
+        $data = json_decode($response->getBody(), true);
+        $this->assertArrayHasKey('message', $data['error']);
+        $this->assertEquals('invalid username or password.', $data['error']['message']);
+    }    
+
+    public function tearDown() {
+        $this->http = null;
+    }  
 }
